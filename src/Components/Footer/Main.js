b95f64942d6a4282ef9b3b17aa1b420cd453f11e
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-10-08 02:55:57 +0600

    footer design created

--- src/Components/Footer/Main.js
+++ src/Components/Footer/Main.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
 const Main = () => {
+
+    const footerData = [
+        {
+        head: "technology",
+        data: ["stack Overflow", "server fault", "Super user", "Web applications","stack Overflow", "server fault", "Super user", "Web applications"]
+    }, 
+        {
+        head: "",
+        data: ["stack Overflow", "server fault", "Super user", "Web applications", "Super user", "Web applications"]
+    }, 
+        {
+        head: "technology",
+        data: ["stack Overflow", "server fault", "Super user", "Web applications","stack Overflow", "server fault", "Super user", "Web applications"]
+    }, 
+        {
+        head: "technology",
+        data: ["stack Overflow", "server fault", "Super user", "server fault", "Super user", "Web applications"]
+    }, 
+        {
+        head: "technology",
+        data: ["stack Overflow", "server fault", "Super user", "Web applications"]
+    }, 
+    {
+        head: "Stack Apps",
+        data: ["Meta Stack Exchange",
+            "Area 51",
+            "Slack Overflow Careers",]
+    }]
+
     return (
         <div className='mx-10 py-10 border-y'>
-            
+            <div className='grid grid-cols-6'>
+                {
+                    footerData.map((f, index) => (<ul className={`text-slate-400 text-sm ${f.head || "mt-5"}`} key={index}>
+                        <li className='uppercase mb-4'>{f.head}</li>
+                        {
+                            f.data.map((d, i) => <li className='cursor-pointer capitalize' key={i}>{d}</li>)
+                        }
+                    </ul>))
+                }
+            </div>
         </div>
     );
 };
